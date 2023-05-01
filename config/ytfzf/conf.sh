e6b3e5b0051e5ec1e7081ec225406db300383171
goiabae <goiabae@protonmail.com>
2023-04-30 23:33:00 -0300

    ytfzf: initial commit

---
+++ config/ytfzf/conf.sh
@@ -0,0 +1,21 @@
+external_menu () {
+  rofi -dmenu -p "$1"
+}
+
+ytdl_pref="247"
+sub_link_count=1
+
+show_thumbnails=1
+thumbnail_viewer="chafa"
+#async_thumbnails=1
+
+#interface="ext"
+notify_playing=1
+custom_scrape_search_exclude="recommended"
+fancy_subs=1
+invidious_instance=https://vid.puffyan.us
+log_level=1
+#is_detach=1
+search_result_type=all
+nsfw=true
+external_menu_len=50
